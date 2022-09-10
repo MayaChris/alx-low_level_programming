699b90eec5de018dee79f949d1a675286ea3db87
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-10 05:46:46 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int no = 0;
+
+	while (no < 10)
+	{
+		printf("%d", no);
+		no++;
+	}
+
+	printf('\n');
+
+	return (0);
+
+}
