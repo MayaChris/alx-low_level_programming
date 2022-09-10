5602f7e3f0ee375dd466cb688cc90e7fe2245b77
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-10 05:38:47 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char az = 'a';
+
+	while (az <= 'z')
+	{
+
+		if (az != 'e' && az != 'q')
+			putchar(az);
+
+		az++;
+	}
+
+	putchar('\n')
+
+	return (0);
+
+}
