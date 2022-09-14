3820af99587d1d6a264987ef114f58c32d19faee
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-14 06:11:23 -0700

    add -isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - This function checks for alphabetics character
+ * @c: Holds character to be checked
+ * Return: shows the result
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
