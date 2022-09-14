53fb6c0149526d4ac9401f35757bb4ae3e3cab36
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-14 06:37:16 -0700

    add add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - This function adds two integers and returns the result
+ * @a: Parameter 1 to be worked on
+ * @b: Parameter 2 to be worked on
+ * Return: prints the sum
+ */
+
+int add(int a, int b)
+{
+	int ab = a + b;
+
+	return (ab);
+}
