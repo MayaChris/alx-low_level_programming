c12488980f1f6fa6dafc2cd549cd42a0cb90cc4d
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-14 05:17:51 -0700

    add putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ab[] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(ab[n]);
+		n++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
