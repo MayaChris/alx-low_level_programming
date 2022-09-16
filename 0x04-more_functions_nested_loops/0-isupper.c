21c8e879bc047d5572e205fbc6b32b09a1ee11db
MayaChris <hannahkalu00@gmail.comgit config --global user.name MayaChriscleargit configgit config --global user.email hannahkalu00@gmail.com>
2022-09-16 07:12:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - this function checks if a character is uppercase or not
- * @c: character to be tested
- * Return: 1 whether it is, 0 otherwise
- */
+*_isupper - checks if a character is uppercase or not
+*@c - character to be checked
+*Return: 1 whether it is, 0 otherwise
+*/
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	for ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
